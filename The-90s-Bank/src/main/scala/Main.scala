00b5839ecae918aa6e3d92129e6e5472d7a56372
Samuel Rodrigues <samuelr@outlook.com>
2024-06-12 23:41:22 +0200

    Added dumb data

--- The-90s-Bank/src/main/scala/Main.scala
+++ The-90s-Bank/src/main/scala/Main.scala
@@ -1,5 +1,32 @@
 object Main extends App {
   printLogo()
+
+  val juca = new Person("Juca Amaro",45)
+  val jucaAccount = new Account("123", juca)
+  var accounts = List[Account]()
+  accounts = accounts :+ jucaAccount
+  val transaction = new Transaction(5,jucaAccount)
+
+  getAccountScreen(accounts)
+
+}
+
+def getAccountScreen(accounts: List[Account]) : Unit = {
+  println("Please type the account number:")
+  val accountNumber = scala.io.StdIn.readLine()
+
+  var account = findAccount(accountNumber, accounts)
+
+  if (account.isEmpty) {
+    println("Account number not found")
+    return
+  }
+
+  println("This account belongs to: " + account.get.person.name)
+}
+
+def findAccount(accountNumber:String, accounts:List[Account]): Option[Account] = {
+  accounts.find(account => accountNumber == account.number)
 }
 
 def printLogo(): Unit = {
